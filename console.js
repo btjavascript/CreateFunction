b7ec2bc7b28c93534b31bab2e91543071654e11f
Hiep0502 <dthiep.gm@gmail.com>
2019-05-13 22:00:27 -0700

    first commit

---
+++ console.js
@@ -0,0 +1,12 @@
+console.log('Hello');
+
+var duck = {
+    name : "Donal",
+    color : 'yealow',
+
+    speak : function(){
+        return'qack quack';
+    }
+};
+
+console.log('Duck ' + duck.name + ' have ' + duck.color + ' and speak '+ duck.speak());
